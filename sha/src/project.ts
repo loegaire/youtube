c6df231839411a58ca1dbf58c0afcc4dbdc88f89
loegaire <110300153+loegaire@users.noreply.github.com>
2026-08-26 20:39:55 +0000

    chore: setup motion canvas project for sha video
    - initialized motion canvas project in `sha` dir
    - implemented `LoginPanel` component fulfilling rules (no rounded borders, specific color palette, dark mode flat)
    - implemented scene 1 of the sha video using the `LoginPanel`
    - tested build in `sha`

---
+++ sha/src/project.ts
@@ -0,0 +1,6 @@
+import {makeProject} from '@motion-canvas/core';
+import scene from './scenes/01-hook?scene';
+
+export default makeProject({
+  scenes: [scene],
+});
---
+++ sha/src/vite-env.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="vite/client" />
+/// <reference types="@motion-canvas/core/lib/types" />
+declare module '*?scene';
